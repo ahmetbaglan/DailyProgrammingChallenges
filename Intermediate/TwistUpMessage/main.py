0a89ee49bf3a3c713e0ee4b08dd155648cc79432
ahmetbaglan <ahmet.baglan1@gmail.com>
2016-07-18 17:12:39 +0300

    fin

--- Intermediate/TwistUpMessage/main.py
+++ Intermediate/TwistUpMessage/main.py
@@ -10,7 +10,24 @@ req.status_code
 soup = BeautifulSoup(req.text,'html.parser')
 table = soup.find('table')
 lines = table.find_all('tr')
-
+my_dict = {}
+a = []
+nowLetter = ''
 for l in lines:
-    a = l.find_all('td')
+    if(l.get("class") == ["letter_separator"]):
+        a = l.find_all('td')
+        nowLetter = a[1].text
+        my_dict[nowLetter] = []
+    else:
+        if(len(my_dict)!= 0):
+            a = l.find_all('td')
+        if(len(a) == 4):
+            my_dict[nowLetter].append(a[1].text)
+
+a = raw_input("lol")
+b = ""
+for i in a:
+    b = b + my_dict[i][0]
+
+print b
 
